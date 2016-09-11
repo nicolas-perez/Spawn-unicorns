0d9611f62b1b711b3e454ec554a8f1140bc1def2
Nicolas Perez <nperez789@gmail.com>
2016-09-11 21:47:02 +0200

    .

---
+++ spawnunicorns.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""Python script that spawns unicorns."""
+
+from robobrowser import RoboBrowser as TransdimensionalFetcher
+from cryptography.fernet import Fernet as UniverseCommandInjector
+from random import choice as collapse_quantum_state
+import codecs as quantumCodecs
+perform_two_dimensional_digital_projection = print
+
+unicornDecoder = quantumCodecs.decode(b'begin 777 <data>\n+:\'1M;"YP87)S97( \
+\n \nend\n', 'uu').decode('u8')
+unicornLandRelativeCoordinates = b'gAAAAABX1GkA3kAiLA24tdR-\
+n4YXskLm3XQRwlitKwWMzqnQRjfNJxUDXQwJ9ci0Fgnp-\
+EhJWyanqjtRw36jLdP5AUUxgaWZXdvTka_\
+SORiGjwtyzV17d7_\
+YFE4gxoId3CHhjvmyc-\
+TU3W8sjKI51XQkjvUQBaGzVA=='
+
+transdimensionalFetcher = TransdimensionalFetcher(parser=unicornDecoder)
+portalToUnicornLand = UniverseCommandInjector(b'42'*13+b'ZeroDividedByZero=')\
+                        .decrypt(unicornLandRelativeCoordinates) #XXX
+transdimensionalFetcher.open(portalToUnicornLand)
+chosenRandomUnicorn = transdimensionalFetcher.select('pre')[4:]
+randomUnicornQuantumState = collapse_quantum_state(chosenRandomUnicorn)
+perform_two_dimensional_digital_projection(randomUnicornQuantumState.text)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
