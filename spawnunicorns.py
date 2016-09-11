a18aa3363b94956549bed0fca28b9b0ce99717f0
Nicolas Perez <nperez789@gmail.com>
2016-09-11 21:54:06 +0200

    .

--- spawnunicorns.py
+++ spawnunicorns.py
@@ -9,8 +9,7 @@ from random import choice as collapse_quantum_state
 import codecs as quantumCodecs
 perform_two_dimensional_digital_projection = print
 
-unicornDecoder = quantumCodecs.decode(b'begin 777 <data>\n+:\'1M;"YP87)S97( \
-\n \nend\n', 'uu').decode('u8')
+unicornDecoder = quantumCodecs.decode(b'begin 777 <data>\n+:\'1M;"YP87)S97( \n \nend\n', 'uu').decode('u8')
 unicornLandRelativeCoordinates = b'gAAAAABX1GkA3kAiLA24tdR-\
 n4YXskLm3XQRwlitKwWMzqnQRjfNJxUDXQwJ9ci0Fgnp-\
 EhJWyanqjtRw36jLdP5AUUxgaWZXdvTka_\
@@ -19,12 +18,13 @@ YFE4gxoId3CHhjvmyc-\
 TU3W8sjKI51XQkjvUQBaGzVA=='
 
 transdimensionalFetcher = TransdimensionalFetcher(parser=unicornDecoder)
-portalToUnicornLand = UniverseCommandInjector(b'42'*13+b'ZeroDividedByZero=')\
-                        .decrypt(unicornLandRelativeCoordinates) #XXX
+portalToUnicornLand = UniverseCommandInjector(b'42'*13+b'ZeroDividedByZero=').decrypt(unicornLandRelativeCoordinates) #XXX
 transdimensionalFetcher.open(portalToUnicornLand)
 chosenRandomUnicorn = transdimensionalFetcher.select('pre')[4:]
 randomUnicornQuantumState = collapse_quantum_state(chosenRandomUnicorn)
 perform_two_dimensional_digital_projection(randomUnicornQuantumState.text)
+
+
 
 
 
